5fa8fbb47c611085962eabe17b1d53bb8b0a8937
Rahul <rahulsharmacs50@gmail.com>
2023-05-13 18:54:21 +0200

    purrrrrrrr

--- src/Statistical_Machine_Learning.py
+++ src/Statistical_Machine_Learning.py
@@ -5,11 +5,22 @@ import numpy as np
 st.set_page_config(layout="wide")
 
 # Using object notation
+col1, col2 = st.columns(2)
+
+
+st.image("logo.png")
 st.title("Statistical Machine Learning")
+
 st.markdown("This is a digital version of the notes I kept for statistical machine learning (ML II) taught by [Prof. Dr. Marius Kloft](https://ml.cs.uni-kl.de) and \
             [Prof. Dr. Sebastian Vollmer](https://sebastian.vollmer.ms) at Rheinland-Pfälzische Technische Universität in Kaiserslautern, RP, Deutschland.")
 
 st.markdown("***")
+
+st.header("Table of contents")
+
+#TODO: Open them in the same page
+st.markdown("[1. Introduction and recap of Machine Learning 1](1._Introduction)")
+st.markdown("")
 
 
 
